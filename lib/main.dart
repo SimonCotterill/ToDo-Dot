f01b5088c5dac8ba3189296a1de1d0eb2bcddc41
Amanda Anderson <aganderson@shaw.ca>
2021-02-02 17:34:50 -0800

    Update main.dart
    Use ToDoAppBar widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,45 +11,8 @@ class Dashboard extends StatelessWidget {
     return Scaffold(
         backgroundColor: todoLightGrey,
         appBar: PreferredSize(
-          preferredSize: Size.fromHeight(80.0),
-          child: AppBar(
-            //https://stackoverflow.com/questions/55826789/flutter-rounded-corners-in-sliverappbar
-              elevation: 25.0,
-              shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(
-                bottom: Radius.circular(5.0),
-              ),
-            ),
-              backgroundColor: todoDarkGreen,
-              centerTitle: true,
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                // Source: https://stackoverflow.com/questions/43981406/how-to-center-the-title-of-an-appbar
-                children: [
-                  //todo: COMMENT: Maybe text or image? both might be a little much
-                  // Removed: Text('Home Page'),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0,10,0,0),
-                    child: Image.asset('assets/To_Do_Light.png',
-                        fit: BoxFit.contain, height: 50),
-                  ),
-                ],
-              ),
-              leading: Padding(
-                padding: const EdgeInsets.fromLTRB(5,12,0,0),
-                child: new IconButton(
-                  icon: Icon(
-                    Icons.menu,
-                    color: todoLightGrey,
-                    size: 40,
-                  ),
-                  onPressed: () {
-                    //do something
-                  },
-                ),
-              )),
-        ),
+            preferredSize: Size.fromHeight(80.0),
+            child: ToDoAppBar(LogoName: 'assets/To_Do_Light.png')),
 
         //Buttons
         body: SafeArea(
@@ -64,13 +27,30 @@ class Dashboard extends StatelessWidget {
                   spacing: 20,
                   runSpacing: 20.0,
                   children: <Widget>[
-                    //Track time
-                    HomeButton(ButtonIcon: Icons.hourglass_top_rounded, TextIcon: "Track Time",),
-                    HomeButton(ButtonIcon: Icons.timer_rounded, TextIcon: "Pomodoro",),
-                    HomeButton(ButtonIcon: Icons.today, TextIcon: "Calendar",),
-                    HomeButton(ButtonIcon: Icons.assignment_rounded, TextIcon: "To-Do List",),
-                    HomeButton(ButtonIcon: Icons.add_rounded, TextIcon: "Connect",),
-                    HomeButton(ButtonIcon: Icons.settings, TextIcon: "Settings",),
+                    HomeButton(
+                      ButtonIcon: Icons.hourglass_top_rounded,
+                      TextIcon: "Track Time",
+                    ),
+                    HomeButton(
+                      ButtonIcon: Icons.timer_rounded,
+                      TextIcon: "Pomodoro",
+                    ),
+                    HomeButton(
+                      ButtonIcon: Icons.today,
+                      TextIcon: "Calendar",
+                    ),
+                    HomeButton(
+                      ButtonIcon: Icons.assignment_rounded,
+                      TextIcon: "To-Do List",
+                    ),
+                    HomeButton(
+                      ButtonIcon: Icons.add_rounded,
+                      TextIcon: "Connect",
+                    ),
+                    HomeButton(
+                      ButtonIcon: Icons.settings,
+                      TextIcon: "Settings",
+                    ),
                   ],
                 ),
               ),
@@ -79,4 +59,3 @@ class Dashboard extends StatelessWidget {
         )));
   }
 }
-
