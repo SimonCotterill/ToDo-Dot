8327cba8a57c3ecc0b74d0306da68cecc943e8e8
Amanda Anderson <aganderson@shaw.ca>
2021-02-02 17:34:40 -0800

    Update style.dart
    Create ToDoAppBar to be a custom widget returning the appbar with identified logo

--- lib/style.dart
+++ lib/style.dart
@@ -8,14 +8,64 @@ Color todoDarkGreen = Color(0xFF214e4c);
 Color todoMediumGreen = Color(0xFF589590);
 Color todoLightGreen = Color(0xFFdce9e8);
 
+// Main App Bar for App
+class ToDoAppBar extends StatelessWidget {
+  final String LogoName;
+
+  const ToDoAppBar({
+    this.LogoName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        //https://stackoverflow.com/questions/55826789/flutter-rounded-corners-in-sliverappbar
+        elevation: 25.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(5.0),
+          ),
+        ),
+        backgroundColor: todoDarkGreen,
+        centerTitle: true,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          // Source: https://stackoverflow.com/questions/43981406/how-to-center-the-title-of-an-appbar
+          children: [
+            //todo: COMMENT: Maybe text or image? both might be a little much
+            // Removed: Text('Home Page'),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: Image.asset(LogoName, fit: BoxFit.contain, height: 50),
+            ),
+          ],
+        ),
+        leading: Padding(
+          padding: const EdgeInsets.fromLTRB(5, 12, 0, 0),
+          child: new IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: todoLightGrey,
+              size: 40,
+            ),
+            onPressed: () {
+              //do something
+            },
+          ),
+        ));
+  }
+}
+
 // Button widget https://www.youtube.com/watch?v=h6OmR0TpWJU&ab_channel=LuisTheTechGuy%21
 class HomeButton extends StatelessWidget {
   final IconData ButtonIcon;
   final String TextIcon;
 
   const HomeButton({
-    this.ButtonIcon, this.TextIcon,
-  }) ;
+    this.ButtonIcon,
+    this.TextIcon,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -25,35 +75,34 @@ class HomeButton extends StatelessWidget {
       child: Card(
         color: todoBlue,
         elevation: 15.0,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(35.0)),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(35.0)),
         child: Center(
             child: Padding(
-              padding: const EdgeInsets.symmetric(
-                  vertical: 20.0, horizontal: 8.0),
-              child: Column(
-                children: <Widget>[
-                  Icon(
-                    ButtonIcon,
-                    color: todoLightGrey,
-                    size: 80.0,
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Text(
-                    TextIcon,
-                    style: TextStyle(
-                        color: todoLightGrey,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0),
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                ],
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 8.0),
+          child: Column(
+            children: <Widget>[
+              Icon(
+                ButtonIcon,
+                color: todoLightGrey,
+                size: 80.0,
               ),
-            )),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                TextIcon,
+                style: TextStyle(
+                    color: todoLightGrey,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+            ],
+          ),
+        )),
       ),
     );
   }
