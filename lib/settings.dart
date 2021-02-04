5ed2e2a37e263888f9241083a720d3289484cebb
Amanda Anderson <aganderson@shaw.ca>
2021-02-03 17:00:52 -0800

    Update settings.dart
    Use custom app bar. Consider adding a place in app bar to add header? Or just put at top of the rest of the page.

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
+import 'style.dart';
 
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title = 'Basic List';
-
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: ListView(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.access_time),
-              title: Text('Timezone'),
-            ),
-            ListTile(
-              leading: Icon(Icons.add_alarm),
-              title: Text('Manage Devices'),
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(80.0),
+          child: ToDoAppBar(LogoName: 'assets/To_Do_Light.png')),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.access_time),
+            title: Text('Timezone'),
+          ),
+          ListTile(
+            leading: Icon(Icons.add_alarm),
+            title: Text('Manage Devices'),
+          ),
+        ],
       ),
     );
   }
